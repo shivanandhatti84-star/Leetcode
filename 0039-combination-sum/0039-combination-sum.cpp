c0eb97a8bc379e4c07d40cdea7a00709b72e6866
shivanandhatti84-star <shivanandhatti84@gmail.com>
2026-08-12 20:23:08 +0530

    Time: 46 ms (12.12%), Space: 46.2 MB (9.44%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void fun(vector<int> c,int target,int ind,vector<vector<int>> *pair,vector<int> *p){
+        if(c.size()==ind||target==0){
+            if(target==0){
+                pair->push_back(*p);
+            }
+            return;
+        }
+        if(c[ind]<=target){
+            p->push_back(c[ind]);
+            fun(c,target-c[ind],ind,pair,p);
+            p->pop_back();
+        }
+        fun(c,target,ind+1,pair,p);
+
+    }
+    vector<vector<int>> combinationSum(vector<int>& c, int target) {
+        vector<vector<int>> pair;
+        vector<int> p;
+        fun(c,target,0,&pair,&p);
+        return pair;
+    }
+};
