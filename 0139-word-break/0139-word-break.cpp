fe566339c72de012c812b5085c5c72692ee1a773
shivanandhatti84-star <shivanandhatti84@gmail.com>
2026-08-15 12:58:40 +0530

    Time: 0 ms (100%), Space: 10.5 MB (88.11%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -5,8 +5,16 @@ public:
         unordered_set<string> dict(wordDict.begin(),wordDict.end());
         vector<bool> dp(n+1,false);
         dp[0]=true;
+        int min_c = INT_MAX;
+int max_c = 0;
+
+for(auto &word : wordDict) {
+    
+    min_c = min(min_c, (int)word.size());
+    max_c = max(max_c, (int)word.size());
+}
         for(int i=1;i<=n;i++){
-            for(int j=0;j<i;j++){
+            for(int j=i-min_c; j >= max(0, i - max_c);j--){
                 if(dp[j]&&dict.count(s.substr(j,i-j))){
                     dp[i]=true;
                     break;
