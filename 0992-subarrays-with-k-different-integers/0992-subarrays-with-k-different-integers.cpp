05f132e847fbb7918a0742d9edd49eca3408870a
shivanandhatti84-star <shivanandhatti84@gmail.com>
2026-09-06 06:26:05 +0530

    Time: 7 ms (96.88%), Space: 44.2 MB (97.3%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,14 +1,47 @@
+// class Solution {
+// public:
+//     int fun(vector<int> &nums,int k){
+//         int r=0,l=0,count=0;
+//         unordered_map<int,int> a;
+//         while(r<nums.size()){
+//             a[nums[r]]++;
+//             while(a.size()>k){
+//                 a[nums[l]]--;
+//                 if(a[nums[l]]==0){
+//                     a.erase(nums[l]);
+//                 }
+//                 l++;
+//             }
+//             count+=r-l+1;
+//             r++;
+//         }
+//         return count;
+//     }
+//     int subarraysWithKDistinct(vector<int>& nums, int k) {
+//         return fun(nums,k)-fun(nums,k-1);
+//     }
+// };
+
+
+
+
+
+
 class Solution {
 public:
     int fun(vector<int> &nums,int k){
         int r=0,l=0,count=0;
-        unordered_map<int,int> a;
+        vector<int> a(nums.size()+1,0);
         while(r<nums.size()){
+            if(a[nums[r]]==0){
+                k--;
+                
+            }
             a[nums[r]]++;
-            while(a.size()>k){
+            while(k<0){
                 a[nums[l]]--;
                 if(a[nums[l]]==0){
-                    a.erase(nums[l]);
+                    k++;
                 }
                 l++;
             }
