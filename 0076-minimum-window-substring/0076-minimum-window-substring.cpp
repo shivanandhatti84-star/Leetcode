d112ac424fb18670b2836f417725441c21838581
shivanandhatti84-star <shivanandhatti84@gmail.com>
2026-09-06 07:04:40 +0530

    Time: 15 ms (35.67%), Space: 11.8 MB (16.07%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char,int> a;
+        for(char p:t){
+            a[p]++;
+        }
+        long long l=0,r=0,count=a.size(),minl=INT_MAX,m=0;
+        unordered_map<char,int> b;
+        while(r<s.size()){
+            b[s[r]]++;
+            if(a.find(s[r])!=a.end()&&a[s[r]]==b[s[r]]){
+                count--;
+            }
+            if(count==0 && minl>r-l+1){
+                minl=min(minl,r-l+1);
+                m=l;
+            } 
+            while(count<=0&&l<=r){
+                b[s[l]]--;
+                if(a.find(s[l]) != a.end() &&b[s[l]]<a[s[l]]){
+                    count++;
+                }
+                l++;
+            if(count==0 && minl>r-l+1){
+                minl=min(minl,r-l+1);
+                m=l;
+            } 
+            }
+            r++;
+        }
+        return minl==INT_MAX?"":s.substr(m,minl);
+    }
+};
